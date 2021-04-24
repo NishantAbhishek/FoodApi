34c24fec0794ebfaadb09dc3f354bf16c51afd6a
nishantabhishek <nishant2548@gmail.com>
2021-04-24 18:00:43 +0530

    Verification Complete

--- core/server.js
+++ core/server.js
@@ -4,6 +4,7 @@ const settings = require("../core/settings");
 const login = require("../controller/login");
 exports.serve = function(){
     http.createServer(function(req,resp){
+        console.log(req.url+" "+req.method);
         switch(req.method){
             case "POST":
                 if(req.url==="/user/login"){
@@ -14,19 +15,25 @@ exports.serve = function(){
                             //todo requst too large
                         }                    
                     });
-                    
+
                     req.on("end",function(){
-                        console.log(reqBody.FirstName);
                         login.sendEmailVerification(req,resp,reqBody);
+                    })
+                }else if(req.url==="/user/authentcate"){
+                    var reqBody = '';
+                    req.on("data",function(data){
+                        reqBody = reqBody+data;
+                        if(reqBody.length>1e7){
+                            //todo requst too large
+                        }
+                    });
+                    req.on("end",function(){
+                        console.log(reqBody);
+                        login.autheticateUser(req,resp,reqBody);
                     })
                 }
                 break;
 
-            case "GET":
-                if(req.url==="/user/authenticate"){
-
-                }
-                break;
         }                
     }).listen(settings.port,function(){
         console.log("Server started on node "+settings.port);
